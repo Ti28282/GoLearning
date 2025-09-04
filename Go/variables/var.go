bab5ac64875ddf0591a2e3b7a8ae19068b5ddf8f
Timur <sabirovt50@gmail.com>
2025-09-04 16:09:50 +0300

    version 0.0.1

---
+++ Go/variables/var.go
@@ -0,0 +1,21 @@
+package main
+
+//
+
+// Data type ( int  string bool float32 )
+// {int = int32}
+// {uint number > 0}
+
+// 1 byte == 8 bit
+
+var test_A uint8 // int8, int16 int32, int64 uint, uint8, uint16, uint32 == uint, uint64
+var test_B string
+var test_C bool
+var test_D float32   // float64
+var test_E complex64 // complex128
+var test_F rune      // == int32 Unicode
+var test_G error     // retrun Error
+var test_H byte      // == uint8 size (1 byte)
+func main() {
+
+}
